70612a565c4efaf93ee36f9590ffc0eed4ebd236
SmoothWay <erakarbay@gmail.com>
2023-04-23 19:47:15 +0600

    update main.py

--- htr/main.py
+++ htr/main.py
@@ -2,25 +2,12 @@ from typing import Tuple, List
 import numpy as np
 import cv2
 
-from dataloader_iam import Batch
-# from model import Model, DecoderType
-# from preprocessor import Preprocessor
-
 from pipeline import read_page, DetectorConfig
 from pipeline.reader import read
 
 from flask import Flask, request, render_template, make_response
 
-class FilePaths:
-    """Filenames and paths to data."""
-    fn_char_list = 'model/charList.txt'
-    fn_summary = 'model/summary.json'
-    fn_corpus = 'model/corpus.txt'
 
-
-def char_list_from_file() -> List[str]:
-    with open(FilePaths.fn_char_list) as f:
-        return list(f.read())
     
 def get_img_height() -> int:
     """Fixed height for NN."""
@@ -32,35 +19,11 @@ def get_img_size(line_mode: bool = False) -> Tuple[int, int]:
         return 256, get_img_height()
     return 128, get_img_height()
 
-
-# def infer(model: Model, img):
-#     """Recognizes text in image provided by file path."""
-#     # img = cv2.imread(fn_img, cv2.IMREAD_GRAYSCALE)
-#     assert img is not None
-
-#     preprocessor = Preprocessor(get_img_size(), dynamic_width=True, padding=16)
-#     img = preprocessor.process_img(img)
-
-#     batch = Batch([img], None, 1)
-#     recognized, probability = model.infer_batch(batch, True)
-#     print(f'Recognized: "{recognized[0]}"')
-#     print(f'Probability: {probability[0]}')
-#     return recognized[0]
-
-
 app = Flask(__name__)
-# decoder_mapping = {'bestpath': DecoderType.BestPath,
-#                     'beamsearch': DecoderType.BeamSearch,
-#                     'wordbeamsearch': DecoderType.WordBeamSearch}
-# decoder_type = decoder_mapping['bestpath']
-
-# model = Model(char_list_from_file(), decoder_type, must_restore=True)
-
 @app.route('/')
 def home():
     return render_template('home.html')
 
-# TODO add full page recognition (segmentate with WordDetectorNN) 
 @app.route('/predict', methods=['POST'])
 def predict():
     # Check if image in request
@@ -85,10 +48,11 @@ def predict():
         read_lines = read_page(img, DetectorConfig(height=1000))
         for read_line in read_lines:
             prediction = ' '.join(read_word.text for read_word in read_line)
-        # else:
+    else:
+        return "Invalid recognition type", 400
         
             
-        # Return the predicted text
+    # Return the predicted text
     return make_response(prediction)
 
 if __name__ == '__main__':
